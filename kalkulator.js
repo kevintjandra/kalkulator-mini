9424ad29827403ecf0c73627c2d636dbddd46a73
kevintjandra <kevinsuryatjandra@gmail.com>
2026-06-26 16:11:30 +0700

    feat: membuat fungsi pengurangan

---
+++ kalkulator.js
@@ -0,0 +1,15 @@
+// file: kalkulator.js
+
+// fungsi untuk melakukan penjumlahan matematika
+
+function tambah(a, b) {
+  return a - b;
+}
+
+// menampilkan hasil ke layar
+console.log("=== APLIKASI KALKULATOR MINI ===");
+let angka1 = 10;
+let angka2 = 5;
+
+let hasil = tambah(angka1, angka2);
+console.log("Hasil pengurangan " + angka1 + " - " + angka2 + " = " + hasil);
